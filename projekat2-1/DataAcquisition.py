a2e77d7edbb8ff33f1368987e516f69cf7dc2519
senpai132 <acanikolic021@gmail.com>
2020-07-06 22:02:19 +0200

    zavrsen 2.1

--- projekat2-1/DataAcquisition.py
+++ projekat2-1/DataAcquisition.py
@@ -22,7 +22,41 @@ def getScaledData():
     return data
 
 def getCustomlyNormalisedData():
-    pass
+    data = read_data()
+    maxi = data['BALANCE'].max()
+    data['BALANCE'] = data['BALANCE'].div(maxi)
+
+    maxi = data['PURCHASES'].max()
+    data['PURCHASES'] = data['PURCHASES'].div(maxi)
+
+    maxi = data['ONEOFF_PURCHASES'].max()
+    data['ONEOFF_PURCHASES'] = data['ONEOFF_PURCHASES'].div(maxi)
+
+    maxi = data['INSTALLMENTS_PURCHASES'].max()
+    data['INSTALLMENTS_PURCHASES'] = data['INSTALLMENTS_PURCHASES'].div(maxi)
+
+    maxi = data['CASH_ADVANCE'].max()
+    data['CASH_ADVANCE'] = data['CASH_ADVANCE'].div(maxi)
+
+    maxi = data['CASH_ADVANCE_TRX'].max()
+    data['CASH_ADVANCE_TRX'] = data['CASH_ADVANCE_TRX'].div(maxi)
+
+    maxi = data['PURCHASES_TRX'].max()
+    data['PURCHASES_TRX'] = data['PURCHASES_TRX'].div(maxi)
+
+    maxi = data['CREDIT_LIMIT'].max()
+    data['CREDIT_LIMIT'] = data['CREDIT_LIMIT'].div(maxi)
+
+    maxi = data['PAYMENTS'].max()
+    data['PAYMENTS'] = data['PAYMENTS'].div(maxi)
+
+    maxi = data['MINIMUM_PAYMENTS'].max()
+    data['MINIMUM_PAYMENTS'] = data['MINIMUM_PAYMENTS'].div(maxi)
+
+    maxi = data['TENURE'].max()
+    data['TENURE'] = data['TENURE'].div(maxi)
+
+    return data
 
 def getRidOfInvalidPostions(data):
     invalid_positions = np.where(np.isnan(data))
